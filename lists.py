896ff88424df38f26c1adae6c73503ded860121a
Dylan Landry <dylan.landry@milkmoovement.com>
2023-07-06 14:35:00 -0400

    first session

---
+++ lists.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3]
+strings = ["hello", "world"]
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+# lists can hold different types
+numbers_and_strings = [123, "banana"]
+print(numbers_and_strings)
---
+++ readme.md
@@ -0,0 +1,3 @@
+Following tutorials on www.learnpython.org
+
+Currently on https://www.learnpython.org/en/Basic_Operators
